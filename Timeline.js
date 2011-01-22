8b94cb2d2bc5a146f0de9bdb0cf52ac92939957a
Joshua Marshall Moore <joshua.marshall.moore@gmail.com>
2011-01-22 10:23:24 -0800

    Resize works once, but not twice.

--- Timeline.js
+++ Timeline.js
@@ -2,11 +2,6 @@
    Timeline.js
 
    (Requires jquery, developed with jquery-1.4.4.min.js in mind)   
-
-   Introducing an object oriented version of the Timeline application.
-   New and improved features shall include:
-   1. travelling forward and backward in time
-   2. multiple calendars
 */
 
 /*
@@ -32,6 +27,11 @@ function Timeline(){
 	alert('back clicked');
     };
 
+    this._testResize = function(){
+	alert('resized');
+    };
+
+
     /*
       timeline.setup()
       Create canvas, back and forward button, as well as slider for scale.
@@ -54,12 +54,73 @@ function Timeline(){
 
 	this._back.onclick = this._backHandler; // set event handler: onclick
 	$('#' + timeline_wrapper_id).append(this._back);
-    };
 
-    /*
-    this.resize = function(){
+
+	this._resizeHandler = function(){
+	    // First, we clear all style so as to prevent duplicates
+	    $('#canvas' + this._id).removeAttr('style');
+	    $('#back' + this._id).removeAttr('style');
+	    // later we'll insert an empty style before modifying the style.
+	    
+	    
+	    // set canvas attributes
+	    
+	    // Width of canvas is window width, with space for borders either 
+	    // side.
+	    var canvas_width = $(window).width() - Timeline._BORDER_SIDES*2;
+	    var canvas_height = $(window).height() * Timeline._HEIGHT_FACTOR;
+	    
+	    
+	    /* The core feature of this block is the z-index. Everything else 
+	       is there because otherwise I can't determine the z-index. At 
+	       least that's what I read somewhere.
+	       
+	       The z-index determines how overlapping elements are drawn.
+	       The higher the z-index, the "closer to the user" the element is.
+	       
+	       In this case, we want to draw everything on top of the canvas,
+	       hence the lowest z-index in our application: 0.
+	    */
+	    $('#canvas'+this._id).attr('style', ''); 
+	    // this undoes our removeAttr('style') from earlier
+
+	    $('#canvas' + this._id).css({
+		width: canvas_width,
+		height: canvas_height,
+		border: '1px solid', // to see what's going on
+		position: 'relative', // "take canvas out of flow"-rough quote
+		top: Timeline._BORDER_TOP, // seems to be the chrome default
+		left: Timeline._BORDER_LEFT, // ditto
+		'z-index': 0
+	    });
+	    
+	    /* Here we define the back button's visual properties. 
+	       Where possible, we calculate them in terms of canvas attributes,
+	       to achieve a consistent layout as the browser is resized.
+	    */
+	    var back_left = $('#canvas' + this._id).css('left') + 'px'; 
+	    // same distance from left timeline_wrapper as canvas
+	    
+	    // This one is a little more difficult: An explanation will follow
+	    // as soon as I've figured it out myself.
+	    var back_top = ((-1)*$('#canvas' + this._id).height() - 6) + 'px';
+	    
+	    $('#back' + this._id).attr('style', ''); 
+	    $('#back' + this._id).css({
+		'background-color': '#336699',
+		width: Timeline._BUTTON_WIDTH,
+		height: $('#canvas' + this._id).height(), // fill canvas height
+		position: 'relative', // same reason as for canvas
+		left: back_left,
+		top: back_top,
+		'z-index': 1
+	    });
+	};
+	
+	
+	this._resizeHandler();
+	$(window).resize(this._resizeHandler);
     };
-    */
 
     /*
       timeline.draw()
@@ -68,60 +129,6 @@ function Timeline(){
       Assumes setup has been called.
      */
     this.draw = function(){
-	// First, we clear all style so as to prevent duplicates
-	$('#canvas' + this._id).removeAttr('style');
-	$('#back' + this._id).removeAttr('style');
-	// later we'll insert an empty style before modifying the style.
-
-	// set canvas attributes
-	
-	// Width of canvas is window width, with space for borders either side.
-	var canvas_width = $(window).width() - Timeline._BORDER_SIDES*2;
-	var canvas_height = $(window).height() * Timeline._HEIGHT_FACTOR;
-
-
-	/* The core feature of this block is the z-index. Everything else is 
-	   there because otherwise I can't determine the z-index. At least 
-	   that's what I read somewhere.
-
-	   The z-index determines how overlapping elements are drawn.
-	   The higher the z-index, the "closer to the user" the element is.
-
-	   In this case, we want to draw everything on top of the canvas,
-	   hence the lowest z-index in our application: 0.
-	 */
-	this._canvas.setAttribute('style', ''); // if this works, it'll undo
-	// our removeAttr('style') from earlier
-	$('#canvas' + this._id).css({
-	    width: canvas_width,
-	    height: canvas_height,
-	    border: '1px solid', // to see what's going on
-	    position: 'relative', // "take canvas out of flow" - rough quote
-	    top: Timeline._BORDER_TOP, // seems to be the chrome default
-	    left: Timeline._BORDER_LEFT, // ditto
-	    'z-index': 0
-	});
-
-	/* Here we define the back button's visual properties. 
-	   Where possible, we calculate them in terms of canvas attributes,
-	   to achieve a consistent layout as the browser is resized.
-	 */
-	var back_left = $('#canvas' + this._id).css('left') + 'px'; 
-	// same distance from left timeline_wrapper as canvas
-
-	// This one is a little more difficult: An explanation will follow
-	// as soon as I've figured it out myself.
-	var back_top = ((-1)*$('#canvas' + this._id).height() - 6) + 'px';
-
-	this._back.setAttribute('style', ''); 
-	$('#back' + this._id).css({
-	    'background-color': '#336699',
-	    width: Timeline._BUTTON_WIDTH,
-	    height: $('#canvas' + this._id).height(), // fill canvas height
-	    position: 'relative', // same reason as for canvas
-	    left: back_left,
-	    top: back_top,
-	    'z-index': 1
-	});
+	console.log('dummy');
     };
 }
